4a00d44da6bf998148905e85679ba0c607548291
Glenn K. Lockwood <glock@lbl.gov>
2016-04-22 14:48:17 -0700

    (1) now handle .tgz'ed files containing stats, and (2) unified dvs and lustre parsing tools

--- diff_concatenated_dvs_stats.py
+++ diff_concatenated_dvs_stats.py
@@ -8,12 +8,14 @@
 
 import re
 import sys
+import tarfile
 import warnings
 
 import pandas
 import procfs.dvs
+import procfs.llite
 
-def diff_single_dvs_stats_file( fp ):
+def diff_single_stats_file( fp, fstype ):
     """
     For a file that contains a series of concatenated /proc/fs/dvs/stats
     outputs, identify counters that change between consecutive dumps
@@ -26,7 +28,10 @@ def diff_single_dvs_stats_file( fp ):
     - We assume pages are not interleaved
     """
 
-    page_list = procfs.dvs.parse_concatenated_dvs_mount_stats(fp)
+    if fstype == "dvs":
+        page_list = procfs.dvs.parse_concatenated_dvs_mount_stats(fp)
+    elif fstype == "llite":
+        page_list = procfs.llite.parse_concatenated_llite_stats(fp)
     counter_diffs = []
 
     ### obtain a list of all observed counters
@@ -43,10 +48,13 @@ def diff_single_dvs_stats_file( fp ):
             for key in all_counters:
                 if key not in a and key in b:
                     val_a = [ 0 for x in b[key] ]
-                elif key in a and key not in b:
-                    val_a = [ 0 for x in b[key] ]
-                val_a = a[key]
-                val_b = b[key]
+                    val_b = b[key]
+                elif key not in b and key in a:
+                    val_a = a[key]
+                    val_b = [ 0 for x in a[key] ]
+                else:
+                    val_a = a[key]
+                    val_b = b[key]
                 page_diff[key] = [ val_b[i] - val_a[i] for i in range( len(val_a) ) ]
             total_change = 0.0
             for key in page_diff.keys():
@@ -60,11 +68,12 @@ def diff_single_dvs_stats_file( fp ):
 
     return counter_diffs
 
-if __name__ == '__main__':
-    # build a super structure of counter diffs per node
-    filenames = sys.argv[1:]
+def ingest_counter_data_filenames( filenames, fstype ):
+    """
+    given a list of file names, build a super structure of counter diffs
+    per node
+    """
     all_counter_data = {}
-    all_counters = set([])
     for filename in filenames:
         rex_match = re.search( '(nid\d+)', filename )
         if rex_match:
@@ -77,9 +86,51 @@ if __name__ == '__main__':
             warnings.warn( "unable to detect node name from %s; using %s" % (filename, nodename) )
         with open( filename, 'r' ) as fp:
             ### only keep first diff since our files contain only one diff
-            all_counter_data[ nodename ] = diff_single_dvs_stats_file( fp )[0] 
-        all_counters = all_counters | set(all_counter_data[ nodename ].keys())
+            all_counter_data[ nodename ] = diff_single_stats_file( fp, fstype )[0] 
+    return all_counter_data
 
+def ingest_counter_data_tgz( tgzfile, fstype ):
+    """
+    given a tar file, build a super structure of counter diffs per node
+    """
+    filenames = []
+    tar = tarfile.open( tgzfile )
+    for tarinfo in tar:
+        if tarinfo.isreg():
+            filenames.append( tarinfo.name )
+
+    all_counter_data = {}
+    for filename in filenames:
+        rex_match = re.search( '(nid\d+)', filename )
+        if rex_match:
+            nodename = rex_match.group(0)
+        else:
+            i = 0
+            while "unknown%d" % i in all_counter_data:
+                i += 1
+            nodename = "unknown%d" % i
+            warnings.warn( "unable to detect node name from %s; using %s" % (filename, nodename) )
+        ### only keep first diff since our files contain only one diff
+        all_counter_data[ nodename ] = diff_single_stats_file( tar.extractfile( filename ), fstype )[0] 
+
+    tar.close()
+    return all_counter_data
+
+if __name__ == '__main__':
+    ### assume we're parsing dvs counters by default
+    if "lustre" in sys.argv[0]:
+        fstype="llite"
+    else:
+        fstype="dvs"
+
+    if sys.argv[1].endswith( 'tgz' ) or sys.argv[1].endswith( '.tar.gz' ):
+        all_counter_data = ingest_counter_data_tgz( sys.argv[1], fstype )
+    else:
+        all_counter_data = ingest_counter_data_filenames( sys.argv[1:], fstype )
+    all_counters = set([])
+    for nodename in all_counter_data.keys():
+        all_counters = all_counters | set(all_counter_data[ nodename ].keys())
+    
     ### Because *_min_max counters encode two useful numbers, we should split
     ### them into two key/value pairs before normalizing.  First loop over
     ### counters whose name end in "_min_max"
