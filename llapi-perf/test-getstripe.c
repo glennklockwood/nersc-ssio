f9c109c99c0e55a9e461c62c9ad8684b9e3d4875
Glenn K. Lockwood <glock@lbl.gov>
2016-05-20 07:54:09 -0700

    clarified the two ioctl calls' syntax and outputs

--- llapi-perf/test-getstripe.c
+++ llapi-perf/test-getstripe.c
@@ -7,39 +7,39 @@
 #include <fcntl.h>
 #include <errno.h>
 
-
 void print_lum( struct lov_user_md *lum );
 
 int main( int argc, char **argv ) {
     int ret;
     int fd;
-    char *input_file= argv[1];
     struct lov_user_md *lum;
     size_t lumsize = sizeof(struct lov_user_md) +
             LOV_MAX_STRIPE_COUNT * sizeof(struct lov_user_ost_data);
 
-
     lum = calloc(1, lumsize);
 
-    printf( "This is with ioctl LL_IOC_LOV_GETSTRIPE:\n" );
-    lum->lmm_magic = LOV_USER_MAGIC;
-    fd = open( input_file, O_RDWR );
-    ret = ioctl( fd, LL_IOC_LOV_GETSTRIPE, (void *)lum );
-    close(fd);
-    print_lum(lum);
-
-    memset(lum, 0, lumsize);
-
-    printf( "\nThis is with llapi_file_get_stripe (which uses IOC_MDC_GETFILESTRIPE):\n" );
-    llapi_file_get_stripe( input_file, lum );
-    print_lum(lum);
-
-    /* argv[1] is the directory containing the file named in argv[2] */
-    if ( argc == 3 ) {
+    /* 
+     * LL_IOC_LOV_GETSTRIPE takes only an open file handle as input
+     *   but doesn't return stripe placement information
+     */
+    if ( argc == 2 ) {
+        char *input_filepath = argv[1];
+        printf( "This is with ioctl LL_IOC_LOV_GETSTRIPE:\n" );
+        lum->lmm_magic = LOV_USER_MAGIC;
+        fd = open( input_filepath, O_RDWR );
+        ret = ioctl( fd, LL_IOC_LOV_GETSTRIPE, (void *)lum );
+        close(fd);
+        print_lum(lum);
+    }
+    /* 
+     * IOC_MDC_GETFILESTRIPE takes an open directory handle and a file basename
+     * string as input and returns both stripe geometry and stripe placement
+     * information
+     */
+    else if ( argc == 3 ) {
+        char *dirname  = argv[1];
         char *basename = argv[2];
-        char *dirname = argv[1];
-        memset(lum, 0, lumsize);
-        printf( "\nThis is with IOC_MDC_GETFILESTRIPE directly:\n" );
+        printf( "This is with IOC_MDC_GETFILESTRIPE:\n" );
         fd = open( dirname, O_RDONLY );
         strcpy( (char *)lum, basename );
         ret = ioctl( fd, IOC_MDC_GETFILESTRIPE, (void*)lum );
