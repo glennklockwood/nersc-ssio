90f865f5447ad136ac51f328e1cab2afdd0f84a7
Glenn K. Lockwood <glock@lbl.gov>
2016-09-19 13:46:10 -0700

    better factoring; handle unaligned data correctly for input strings

--- hoover/sha1.c
+++ hoover/sha1.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <openssl/sha.h>
+#include <string.h>
 
-int main()
-{  
-    unsigned const char str[] = "hello world";
-    unsigned char hash[SHA_DIGEST_LENGTH]; // == 20
+/* sha1_blob: Turn a byte blob into a hexified sha1 sum.  Not thread safe.
+ *
+ * input: blob - a byte array
+ *        len - length of that byte array
+ * output: SHA1 hash with each byte expressed as a hex value
+ *
+ * Note that hashing strings should NOT include the terminal NULL
+ * byte since that is a C construct.  Also be careful when using
+ * strlen() vs. sizeof(); sizeof() includes padding bytes if blob
+ * is not aligned to 8 bytes.
+ */
+unsigned char *sha1_blob( unsigned const char *blob, size_t len ) {
+    unsigned char hash[SHA_DIGEST_LENGTH];
+    static unsigned char hex_hash[SHA_DIGEST_LENGTH * 2 + 1];
     int i;
 
-    SHA1(str, sizeof(str) - 1, hash);
+    SHA1(blob, len, hash);
 
-    printf( "Original string: [%s]\nSHA1 hash: [", str );
-    for (i = 0;i < SHA_DIGEST_LENGTH; i++) {
-        printf( "%02x", hash[i] );
+    /* note that hash does not terminate in \0 */
+    for ( i = 0; i < SHA_DIGEST_LENGTH; i++ )
+        sprintf( (char*)&(hex_hash[2*i]), "%02x", hash[i] );
+
+    return hex_hash;
+}
+
+int main( int argc, char **argv )
+{
+    if ( argc < 2 ) {
+        fprintf( stderr, "Syntax: %s <input string>\n", argv[0] );
+        return 1;
     }
-    printf("]\n");
 
+    printf( "Original string: [%s]\nSHA1 hash:       [%s]\n",
+        argv[1],
+        sha1_blob( (unsigned const char*)argv[1], strlen(argv[1]) ) );
     return 0;
 }
